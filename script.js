8edeeda855ca0c93c2f96dc740435374ba4ec240
Islom <Islom>
2022-11-23 17:00:27 +0500

    JS3

---
+++ script.js
@@ -0,0 +1,11 @@
+var yosh = +prompt("Sonlar bilan yoshini kirit - Fol ochib beraman")
+
+if (yosh >=150) {
+    alert("Doda vohtiz keb qopti")
+} else if (yosh >= 60) {
+    alert("Tayorgarli korar ekansiz amaki")
+}else if (yosh >= 18){
+    alert("Molde yotmastan turib ishla")
+}else  {
+    alert("San hali yosh bola ekansanu , Uyatsiz narsa kormastan oqi")
+}
